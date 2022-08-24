db08f95f4ed13f351ed0a44000c18a29d82756b9
ali.rabiee <a.rabiee@fidilio.com>
2022-08-24 18:46:29 +0430

    isg / ssr

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
-function HomePage() {
+import path from 'path'
+import fs from 'fs/promises'
+
+function HomePage(props) {
+
+  const { products } = props
+
   return (
     <div>
       <h1>This Is The Home Page!</h1>
+      <ul>
+        {products?.map(p => (<li key={p.id}>{p.title}</li>))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export async function getStaticProps() {
+
+  const filePath = path.join(process.cwd(), 'data', 'db.json')
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData)
+  return {
+    props: {
+      products: data.products
+    },
+    revalidate: 10
+  }
+}
+
+export default HomePage
